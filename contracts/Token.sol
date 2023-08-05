67dd52ab7da892208236507c62248b3a032a372c
Simon <simon72217@gmail.com>
2023-08-05 15:54:22 +0700

    a

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,8 +8,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Token is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
+    mapping(address => uint256) lastTimeTx;
+    uint256 limitTimeTx = 10 * 60 ;
+    mapping(address => bool) pools;
+
     constructor() ERC20("Token", "MTK") ERC20Permit("Token") {
-        _mint(msg.sender, 1_000_000_000_000 * 10 ** decimals());
+        _mint(msg.sender, 1_000_000_000_000 * 10**decimals());
     }
 
     function pause() public onlyOwner {
@@ -24,11 +28,35 @@ contract Token is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        // if buy or sell, "from" or "to" limit time
+        if (
+            (pools[from] =
+                true &&
+                (lastTimeTx[to] + limitTimeTx) > block.timestamp) ||
+            (pools[to] =
+                true &&
+                (lastTimeTx[from] + limitTimeTx) > block.timestamp)
+        ) {
+            revert("You are bot fast trade");
+        }
+
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function time() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function changeLimitTimeTx(uint256 _limitTimeTx) public onlyOwner {
+        limitTimeTx = _limitTimeTx;
+    }
+
+    function setPools(address _pool) public onlyOwner {
+        pools[_pool] = true;
     }
 }
